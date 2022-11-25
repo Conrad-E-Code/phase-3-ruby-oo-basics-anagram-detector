2e7019d721cc55a302dbaa18593bf1bcde238444
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-25 16:12:55 -0700

    commit tree 23682ddafbe20939e000ad6dcb16457e9fc3e7d8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,36 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+    def initialize(word)
+        @word = word
+    end
+    def match word_array
+        word_array.select do |word|
+            if word.chars.sort == @word.chars.sort
+                word
+            end
+        end
+    end
+
+    # def match word_array
+    #     stringstoArrs = word_array.map do |word|
+    #         word.split("")
+    #     end
+        # matchwords = stringstoArrs.select do |splitword|
+        #     @word.split("").include? splitword
+        # end
+        # sortedstringstoArrs = stringstoArrs.map do |word|
+        #     word.sort
+        # end
+        # matchletterwords = sortedstringstoArrs.map do |word|
+        #     word.join("")  
+        # end 
+        # matchletterwords.select do |word|
+        #     word.include?(@word.split("").sort().join(""))
+        # end
+end
+
+
+# bobcat = Anagram.new("bobcat")
+# binding.pry
+# bobcat.match ["catbob", "frank", "boctab"]
